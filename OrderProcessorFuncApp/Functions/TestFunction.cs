9be5039c5a22f724327569191cf365b672603577
Cheranga Hatangala <cheranga@gmail.com>
2025-05-05 12:21:12 +1000

    chore: rewriting functions

---
+++ OrderProcessorFuncApp/Functions/TestFunction.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace OrderProcessorFuncApp.Functions;
+
+public class TestFunction
+{
+    private readonly ILogger _logger;
+
+    public TestFunction(ILogger<TestFunction> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function(nameof(TestFunction))]
+    public async Task<HttpResponseData> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        FunctionContext context)
+    {
+        _logger.LogInformation("HelloWorld function processed a request.");
+
+        var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+        await response.WriteStringAsync("Hello from Azure Functions (isolated)!");
+        return response;
+    }
+}
